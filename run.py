c4b7d27a0416a5e7aeace48964dbb22c9931a634
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-09-16 13:56:23 +0000

    feat: add 'while' loop for game introduction

--- run.py
+++ run.py
@@ -7,6 +7,21 @@ COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 ROWS = range(1, 9)
 
 def generate_grid():
+    print("\nWelcome to Battleships!\n")
+    # while loop runs until player provides a valid input
+    while True:
+        # provides player input field and converts text to lowercase to match the checks below
+        game = input("Would you like to play a game? ('y'/'n')").lower()
+        if game == "n":
+            print("Maybe another time. Goodbye!")
+            quit()
+        if game == "y":
+            print("\nLet's play\n")
+            break
+        else:
+            print("That is not a valid input. Please try again.")
+            continue
+
     # print the column headers A - H
     print("   " + "  ".join(COLUMNS)) 
     # print each row beginning with row numbers
